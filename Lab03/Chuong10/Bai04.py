f11dfa56ff744e1a4879e4c0e60b7e0f7e1a542e
TuongTuan3T <tuongtuantran@gmail.com>
2021-10-06 20:10:57 +0700

    Update Bai04.py
    update câu 4 lab 03

--- Lab03/Chuong10/Bai04.py
+++ Lab03/Chuong10/Bai04.py
@@ -1,5 +1,30 @@
-f = open('d:/python nc/PythonNC/Lab03/Chuong10/alkaline_metals.txt', 'r')
-f.seek(13,0)
-line = f.readline()
-print(line)
-f.close()
+def skip_header(reader):
+    '''Skip the header in reader r, and return the first
+    real piece of data.'''
+    
+    # Read the description line and then the comment lines.
+    line = reader.readline()    
+    line = reader.readline()
+    while line.startswith('#'):
+        line = reader.readline() 
+    # Now line contains the first real piece of data.
+    return line
+
+def process_file(reader):
+    '''(file open for reading) -> NoneType
+    Read and print the data from reader, which must start with a single
+    description line, then a sequence of lines beginning with '#', 
+    then a sequence of data.
+    '''
+
+    # Find the first piece of data.
+    line = skip_header(reader).strip()
+    print(line)
+    # Read the rest of the data.
+    for line in reader:
+        line = line.strip()
+        print(line)
+
+input_file = open('d:/python nc/PythonNC/Lab03/Chuong10/alkaline_metals.txt', 'r')
+process_file(input_file)
+input_file.close()
