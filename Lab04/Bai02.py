ff8715f22d599d83397fb2568629a050b044f67f
TuongTuan3T <tuongtuantran@gmail.com>
2021-10-12 09:47:12 +0700

    Create Bai02.py
    Lab04 Bai02

---
+++ Lab04/Bai02.py
@@ -0,0 +1,9 @@
+def mating_pairs(males, females):
+  pairs = set()
+  num_gerbils = len(males)
+  for i in range(num_gerbils):
+    male = males.pop()
+    female = females.pop()
+    pairs.add((male, female),)
+  return pairs
+mating_pairs({'Anne', 'Beatrice', 'Cari'}, {'Ali', 'Bob', 'Chen'})
